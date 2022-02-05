21374c90bd59ce17a5dd8690e17e5e632440e215
Ar Fire <72059150+arfire333@users.noreply.github.com>
2022-02-05 12:38:25 -0500

    Completed Lesson 3.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const appa = Vue.createApp({
   data() {
     return {
       product: 'Socks',
+      url: 'https://duckduckgo.com/socks',
+      image: './assets/images/socks_blue.jpg',
       description: 'Something to keep your feet warm!'
     }
   }
