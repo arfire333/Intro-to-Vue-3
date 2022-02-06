4504a504dd813cfc545b7fb2417d0fb8bb9ef6f2
Ar Fire <72059150+arfire333@users.noreply.github.com>
2022-02-05 19:46:57 -0500

    Completed lesson 7.

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const appa = Vue.createApp({
+const app = Vue.createApp({
   data() {
     return {
       cart: 0,
       product: 'Socks',      
       image: './assets/images/socks_green.jpg',
-      inventory: 100,      
+      inventory: 11,      
       details: ['50% Cotton','30% Wool','20% Polyester'],      
       variants: [
-        { id: 2234, color: 'Green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'Blue', image: './assets/images/socks_blue.jpg' },
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
       ],
       description: 'Something to keep your feet warm!',
       sizes: ['Small', 'Medium', 'Large'],
@@ -17,11 +17,17 @@ const appa = Vue.createApp({
     }
   },
   methods: {
+    inStock() {      
+      return this.inventory > 0;      
+    },
+    cartEmpty() {
+      return this.cart == 0;
+    },
     addToCart() {
       this.cart += 1;
     },
     removeFromCart() {
-      this.cart -= this.cart > 0 ? 1 : 0;      
+      this.cart -= ( this.cart > 0 ? 1 : 0 );      
     },
     updateImage(image){
       this.image = image;
