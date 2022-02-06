fe7dc798b02d44c68abe8e9d910f3f979cd9ac33
Ar Fire <72059150+arfire333@users.noreply.github.com>
2022-02-05 21:57:54 -0500

    Completed Lesson 8.

--- main.js
+++ main.js
@@ -3,35 +3,41 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: 'Socks',      
-      image: './assets/images/socks_green.jpg',
+      brand: 'Vue Mastery',
+      selectedVariant: 0, // './assets/images/socks_green.jpg',
       inventory: 11,      
       details: ['50% Cotton','30% Wool','20% Polyester'],      
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false },
       ],
       description: 'Something to keep your feet warm!',
-      sizes: ['Small', 'Medium', 'Large'],
-      onSale: true,
+      sizes: ['Small', 'Medium', 'Large'],      
       url: 'https://duckduckgo.com/socks',      
     }
   },
-  methods: {
-    inStock() {      
-      return this.inventory > 0;      
-    },
-    cartEmpty() {
-      return this.cart == 0;
-    },
+  methods: {    
+
     addToCart() {
       this.cart += 1;
     },
     removeFromCart() {
       this.cart -= ( this.cart > 0 ? 1 : 0 );      
     },
-    updateImage(image){
-      this.image = image;
+    updateVariant(index){
+      this.selectedVariant = index;
     }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product + ( this.variants[this.selectedVariant].onSale ? " is on sale!" : "")
+    },
+    inStock() {      
+      return this.variants[this.selectedVariant].quantity > 0;      
+    },
+    cartEmpty() {
+      return this.cart == 0;
+    },
   }
 });
 
