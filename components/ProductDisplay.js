1e4ddc4cefee7302d06d43f9c14dd6a13dd8c9fe
Ar Fire <72059150+arfire333@users.noreply.github.com>
2022-02-06 21:00:09 -0500

    Completed Lesson 9.

---
+++ components/ProductDisplay.js
@@ -0,0 +1,82 @@
+app.component('product-display', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template:
+    /*html*/
+    `
+    <div class="product-display">
+      <div class="product-container" >
+        <div class="product-image">
+          <img :class="{'out-of-stock-img': !inStock }" v-bind:src="variants[selectedVariant].image" v-bind:alt="product + ' Picture'" :title="product + ' Picture'" />
+        </div>
+        <div class="product-info">
+          <h1>{{ title }}</h1>
+          <p v-if="inStock">In Stock</p>
+          <p v-else-if="variants[selectedVariant].quantity > 0">Almost sold out!</p>
+          <p v-else>Out of Stock</p>          
+          <p>Shipping: {{shipping}}</p>
+          
+          <p>{{ description }}</p>
+
+          <product-details :details="details"></product-details>
+
+          <div v-for="(variant, index) in variants" :key="variant.id" @mouseover="updateVariant(index)"
+          class="color-circle" :style="{backgroundColor: variant.color, borderColor: variant.color }">
+        </div>
+        <table>
+          <tr>
+            <td><button class="button" :class="{disabledButton: !inStock }" @click="addToCart" :disabled="!inStock">Add to Cart</button></td>
+            <td><button class="button" :class="{disabledButton: cartEmpty }" @click="removeFromCart" :disabled="cartEmpty">Remove One</button> </td>
+          </tr>
+        </table >
+      </div >
+    </div >`,
+  data() {
+    return {      
+      product: 'Socks',
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
+      inventory: 11,
+      details: ['50% Cotton', '30% Wool', '20% Polyester'],
+      variants: [
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false },
+      ],
+      description: 'Something to keep your feet warm!',
+      sizes: ['Small', 'Medium', 'Large'],
+      url: 'https://duckduckgo.com/socks',
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      this.cart -= (this.cart > 0 ? 1 : 0);
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity > 0;
+    },
+    shipping() {
+      if (this.premium){
+        return 'Free'
+      }else{
+        return 2.99
+      }
+    }
+  }
+
+
+})
--- main.js
+++ main.js
@@ -2,42 +2,11 @@ const app = Vue.createApp({
   data() {
     return {
       cart: 0,
-      product: 'Socks',      
-      brand: 'Vue Mastery',
-      selectedVariant: 0, // './assets/images/socks_green.jpg',
-      inventory: 11,      
-      details: ['50% Cotton','30% Wool','20% Polyester'],      
-      variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false },
-      ],
-      description: 'Something to keep your feet warm!',
-      sizes: ['Small', 'Medium', 'Large'],      
-      url: 'https://duckduckgo.com/socks',      
+      premium: true
     }
   },
   methods: {    
-
-    addToCart() {
-      this.cart += 1;
-    },
-    removeFromCart() {
-      this.cart -= ( this.cart > 0 ? 1 : 0 );      
-    },
-    updateVariant(index){
-      this.selectedVariant = index;
-    }
   },
-  computed: {
-    title() {
-      return this.brand + ' ' + this.product + ( this.variants[this.selectedVariant].onSale ? " is on sale!" : "")
-    },
-    inStock() {      
-      return this.variants[this.selectedVariant].quantity > 0;      
-    },
-    cartEmpty() {
-      return this.cart == 0;
-    },
-  }
+
 });
 
